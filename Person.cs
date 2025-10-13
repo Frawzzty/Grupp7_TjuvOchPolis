3809bf6db9e74b1230235e42eeb1ab3c2b9bd372
Alex Hammarstedt <alexhammarstedt1998@gmail.com>
2025-10-13 14:55:56 +0200

    Added Person class

---
+++ Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp7_TjuvOchPolis
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int[] Position { get; set; }
+        public int[] Direction { get; set; } //X -1, Y 0
+        //public List<Item> Inventroy { get; set; } //Skapa Item klass
+
+        public Person(string name, int[] position, int[] direction)
+        {
+            Name = name;
+            Position = position;
+            Direction = direction;
+        }
+
+
+        public void Move() //Move Person. call function in canvas
+        {
+            //Can move?
+        }
+
+    }
+}
