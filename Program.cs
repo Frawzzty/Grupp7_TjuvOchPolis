dae52516a9a6888e3460c112b894e8078576b83d
Alex Hammarstedt <alexhammarstedt1998@gmail.com>
2025-10-13 14:24:07 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Grupp7_TjuvOchPolis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
